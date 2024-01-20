f59aac7267478c6ee58f29ed3d014c77cec92142
Xun Li <lxfind@gmail.com>
2024-01-19 20:55:33 -0800

    Use single storage

--- src/config.rs
+++ src/config.rs
@@ -9,18 +9,21 @@ use sui_config::Config;
 use sui_types::crypto::{get_account_key_pair, SuiKeyPair};
 
 pub const DEFAULT_RPC_PORT: u16 = 9527;
+pub const DEFAULT_METRICS_PORT: u16 = 9528;
 pub const LOCALHOST: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
 
 #[serde_as]
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct GasStationConfig {
+    // TODO: Make this a vector.
     pub keypair: SuiKeyPair,
-    pub local_db_path: PathBuf,
     pub rpc_host_ip: Ipv4Addr,
     pub rpc_port: u16,
     pub gas_pool_config: GasPoolStorageConfig,
     pub fullnode_url: String,
+    pub target_init_coin_balance: u64,
+    pub run_coin_expiring_task: bool,
 }
 
 impl Default for GasStationConfig {
@@ -28,11 +31,13 @@ impl Default for GasStationConfig {
         let (_, keypair) = get_account_key_pair();
         GasStationConfig {
             keypair: keypair.into(),
-            local_db_path: tempfile::tempdir().unwrap().into_path(),
             rpc_host_ip: LOCALHOST,
             rpc_port: DEFAULT_RPC_PORT,
             gas_pool_config: GasPoolStorageConfig::default(),
             fullnode_url: "http://localhost:9000".to_string(),
+            // 0.01 SUI.
+            target_init_coin_balance: 10000000,
+            run_coin_expiring_task: true,
         }
     }
 }
@@ -40,14 +45,13 @@ impl Default for GasStationConfig {
 #[serde_as]
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case")]
-pub enum GasPoolStorageConfig {
-    LocalRocksDbForTesting { db_path: PathBuf },
-    RemoteRocksDb { db_rpc_url: String },
+pub struct GasPoolStorageConfig {
+    pub db_path: PathBuf,
 }
 
 impl Default for GasPoolStorageConfig {
     fn default() -> Self {
-        GasPoolStorageConfig::LocalRocksDbForTesting {
+        Self {
             db_path: tempfile::tempdir().unwrap().into_path(),
         }
     }
