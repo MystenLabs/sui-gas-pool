918e9d370cd1ae11e7137199fd54f33b80e9e689
Parshant Singh <parshant.e2@gmail.com>
2024-09-10 19:07:45 +0100

    Added sentry to gas pool

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,18 @@
 use clap::Parser;
 use sui_gas_pool::command::Command;
 
-#[tokio::main]
-async fn main() {
-    let command = Command::parse();
-    command.execute().await;
+fn main() {
+    let _guard = sentry::init(("https://a4d7d57ce55195bb2c8df9c2bb7e8154@o4507907608150016.ingest.de.sentry.io/4507929897140304", sentry::ClientOptions {
+        release: sentry::release_name!(),
+        ..Default::default()
+      }));
+
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            let command = Command::parse();
+            command.execute().await;
+        });
 }
