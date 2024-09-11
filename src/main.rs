64459308ccc29e0d106122a0322f3e928e113e63
Parshant Singh <parshant.e2@gmail.com>
2024-09-12 00:37:22 +0100

    minor fix

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use sui_gas_pool::command::Command;
 fn main() {
     let _guard = sentry::init(("https://a4d7d57ce55195bb2c8df9c2bb7e8154@o4507907608150016.ingest.de.sentry.io/4507929897140304", sentry::ClientOptions {
         release: sentry::release_name!(),
+        attach_stacktrace: true,
         ..Default::default()
       }));
 
