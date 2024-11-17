b3a020c4d59c9e60de88e3769d0026b7e039aec6
Parshant Singh <parshant.e2@gmail.com>
2024-11-17 21:08:19 +0000

    Add script

---
+++ run_sui_gas_pool.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start haproxy in background
+/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg
+
+# Start promtail in background
+/usr/local/bin/promtail -config.file=/etc/promtail/config.yml &
+
+# Start sui gas pool
+/usr/local/bin/sui_gas_pool --config-path /usr/local/bin/testnet-prod.yaml
